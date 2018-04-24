da87b6b423863b90589c73485eec509342360691
Jenna Pinkham <pinkhamjenna@gmail.com>
2018-04-23 21:03:06 -0700

    layout and styling

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div id="words">
-        <p>clicky dot</p>
-        <span id="points"></span>
-        <span id="lives"></span>
-        <span id="high-score"></span>
-    </div>
-    
-    <div id="game-end" class="hidden-div">
-        <span>you died, loser :(</span>
-        <button id="try-again">try again?</button>
-    </div>
-    
     <div id="game-container" class="col-8"></div>
+    
+    <div id="UI">
+        <h1>clicky dot</h1>
+        <p><span id="points"></span></p>
+        <p><span id="lives"></span></p>
+        <p><span id="high-score"></span></p>
+
+        <div id="game-end" class="hidden-div">
+            <p>you died, loser :(</p>
+            <button id="try-again">try again?</button>
+        </div>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="script.js"></script>
